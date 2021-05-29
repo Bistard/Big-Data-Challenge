498d2534814ca3eb341dfbb3e8b5cbe9cc9c58be
Bistard <38385498+Bistard@users.noreply.github.com>
2021-05-29 14:33:59 +0800

    update fake news per day 0-basic.png

--- dev/google_vs_fake.py
+++ dev/google_vs_fake.py
@@ -24,11 +24,33 @@ dir_name = os.path.dirname(__file__)
 fake_news_path = os.path.join(dir_name, 'fake_news/0-basic.csv')
 google_trend_path = os.path.join(dir_name, 'google_trend/0-basic.csv')
 
+fig0_path = os.path.join(dir_name, 'fake_news/1-basic.png')
+
 df_fake = pd.read_csv(fake_news_path)
 df_google = pd.read_csv(google_trend_path)
 
 df_fake
+df_google
 
-# df_google
+# %%
+dates = []
+freqs = []
+for i, day in enumerate(df_fake['date']):
+    d = dt.datetime.strptime(day, '%Y-%m-%d').date()
+    if d.year == 2020 and d.month == 11:
+        break
+    dates.append(d)
+    freqs.append(df_fake['frequency'][i])
 
+plt.figure(figsize=(20, 10))
+plt.plot(dates, freqs, color='red', linewidth=2)
+
+# TODO: more specific titles and legend
+plt.legend(['fake news per day'])
+plt.xlabel('Time')
+plt.ylabel('Search Interest')
+plt.title('fake news')
+fig0 = plt.gcf()
+plt.show()
+fig0.savefig(fig0_path)
 # %%
